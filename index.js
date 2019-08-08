a048a7419a18ffb0404e1eb48bfe3d2777b0446a
MuneebUrRehman <munni7474@gmail.com>
2019-08-08 22:25:28 +0500

    optimize restaurant code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $(".header-user-icon").click(function(){
-
         $(".login-overlay").fadeIn();
-
     });
     $( ".login-overlay" ).on( 'click', function( event ) {
         $( this ).fadeOut();
@@ -13,12 +11,9 @@ $(document).ready(function(){
         event.stopPropagation();
     });
 
-
     $(".login-box-header .right-panel").click(function(){
-
         $(".login-overlay").fadeOut();
         $(".login-overlay input[type=text] , input[type=password]").val("");
-
     });
 
     loadCities();
@@ -32,15 +27,16 @@ function loadCities() {
         contentType: "application/json;charset=utf-8",
         success: function (result) {
             var html='';
-            
 			
              if(result) {
                 $.each(result, function(index,city){
                     html += '<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3" data-aos="fade-up" data-aos-duration="2000">';
                     html +='<div class="four-panel">';
                     html += '<div class="city-icon">';
+                    html += '<a href="restaurant.html?id=' + city.Id + '" target="_blank">';
                     html += '<img src="'+IP+":"+PORT+"/"+city.ImagePath+'" />';
                     html += '<span>' + city.Name + '</span>'
+                    html += '</a>'
                     html += '</div></div></div>'
                 }); 
                 $("#cityHtml").html(html);
